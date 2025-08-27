16f1470ad99ea610e79f2717b7cfb64560328f19
Yuming <yumingh7@gmail.com>
2025-08-27 15:26:53 -0400

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7BFE8E9ET"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C7BFE8E9ET');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
