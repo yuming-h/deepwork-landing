cfe5c60f2be34031508dfc133dfa442c051108c7
Yuming <yumingh7@gmail.com>
2025-09-03 21:13:06 -0400

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,10 +40,31 @@ export default function RootLayout({
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq)return; n=f.fbq=function(){n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+                if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0'; n.queue=[];
+                t=b.createElement(e); t.async=!0; t.src=v; s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)
+              }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1073731454744827');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1073731454744827&ev=PageView&noscript=1"
+          />
+        </noscript>
         {children}
       </body>
     </html>
